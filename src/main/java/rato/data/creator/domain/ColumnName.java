edcfe225a4e62322590b591f4ea2238ba02a4499
Toshiya Kimura <tosikimu@gmail.com>
2014-06-19 19:38:13 +0900

    equalsメソッドとhashCodeメソッドをオーバーライド

--- src/main/java/rato/data/creator/domain/ColumnName.java
+++ src/main/java/rato/data/creator/domain/ColumnName.java
@@ -2,6 +2,8 @@ package rato.data.creator.domain;
 
 import java.util.Objects;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.seasar.doma.Domain;
 
 /**
@@ -35,6 +37,26 @@ public class ColumnName {
 	 */
 	public String getValue() {
 		return this.value;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object paramObject) {
+		return EqualsBuilder.reflectionEquals(this, paramObject);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 	/*
