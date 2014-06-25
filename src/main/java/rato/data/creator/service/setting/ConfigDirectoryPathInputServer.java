12405c6872bcf480a01d1b3b2d93e213e37571ed
Toshiya Kimura <tosikimu@gmail.com>
2014-06-25 11:43:25 +0900

    設定ファイルの配置してあるディレクトリパスを処理するサービスクラスを作成

---
+++ src/main/java/rato/data/creator/service/setting/ConfigDirectoryPathInputServer.java
@@ -0,0 +1,39 @@
+package rato.data.creator.service.setting;
+
+import java.util.ResourceBundle;
+
+import rato.data.creator.bo.CommandLineServiceResultBo;
+import rato.data.creator.bo.InputValue;
+import rato.data.creator.service.BaseCommandLineService;
+
+/**
+ * <p>
+ * 設定ファイルの配置してあるディレクトリパスを処理するサービスクラスです。
+ * </p>
+ * @author toshiya
+ *
+ */
+public class ConfigDirectoryPathInputServer extends BaseCommandLineService {
+
+	@Override
+	protected String getQuestionMessage(
+			CommandLineServiceResultBo beforeResult, ResourceBundle bundle) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	protected void validateProcess(CommandLineServiceResultBo beforeResult,
+			InputValue inputValue) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected CommandLineServiceResultBo mainProcess(
+			CommandLineServiceResultBo beforeResult, InputValue inputValue) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
